cbea30c29633e00ab4e365d85487e251e2756d09
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2007-07-20 01:12:43 +0200

    Make torrent_peer into a gen_server.

--- lib/etorrent-1.0/src/torrent_peer.erl
+++ lib/etorrent-1.0/src/torrent_peer.erl
@@ -1,157 +1,136 @@
 %%%-------------------------------------------------------------------
 %%% File    : torrent_peer.erl
 %%% Author  : Jesper Louis Andersen <>
-%%% Description : Representation of a torrent peer.
+%%% Description : Represents a peer
 %%%
-%%% Created : 18 Jul 2007 by Jesper Louis Andersen <>
+%%% Created : 19 Jul 2007 by Jesper Louis Andersen <>
 %%%-------------------------------------------------------------------
 -module(torrent_peer).
 
--behaviour(gen_fsm).
+-behaviour(gen_server).
 
 %% API
--export([start_link/3, connect/1]).
+-export([start_link/4, connect/2]).
 
-%% gen_fsm callbacks
--export([init/1, initial/2, initial/3, handle_event/3,
-	 handle_sync_event/4, handle_info/3, terminate/3, code_change/4]).
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+	 terminate/2, code_change/3]).
 
 -record(state, { ip = none,
 		 port = none,
-		 peer_id = none }).
+		 peer_id = none,
+		 info_hash = none,
+		 tcp_socket = none}).
+
+-define(DEFAULT_CONNECT_TIMEOUT, 120000).
+
 
 %%====================================================================
 %% API
 %%====================================================================
 %%--------------------------------------------------------------------
-%% Function: start_link() -> ok,Pid} | ignore | {error,Error}
-%% Description:Creates a gen_fsm process which calls Module:init/1 to
-%% initialize. To ensure a synchronized start-up procedure, this function
-%% does not return until Module:init/1 has returned.
+%% Function: start_link() -> {ok,Pid} | ignore | {error,Error}
+%% Description: Starts the server
 %%--------------------------------------------------------------------
-start_link(IP, Port, PeerId) ->
-    gen_fsm:start_link(?MODULE, [IP, Port, PeerId], []).
+start_link(IP, Port, PeerId, InfoHash) ->
+    gen_server:start_link(?MODULE, [IP, Port, PeerId, InfoHash], []).
 
-connect(Pid) ->
-    gen_fsm:send_event(Pid, connect).
+connect(Pid, MyPeerId) ->
+    gen_server:cast(Pid, {connect, MyPeerId}).
 
 %%====================================================================
-%% gen_fsm callbacks
+%% gen_server callbacks
 %%====================================================================
-%%--------------------------------------------------------------------
-%% Function: init(Args) -> {ok, StateName, State} |
-%%                         {ok, StateName, State, Timeout} |
-%%                         ignore                              |
-%%                         {stop, StopReason}
-%% Description:Whenever a gen_fsm is started using gen_fsm:start/[3,4] or
-%% gen_fsm:start_link/3,4, this function is called by the new process to
-%% initialize.
-%%--------------------------------------------------------------------
-init([IP, Port, PeerId]) ->
-    {ok, initial, #state{ ip = IP,
-			  port = Port,
-			  peer_id = PeerId}}.
 
 %%--------------------------------------------------------------------
-%% Function:
-%% state_name(Event, State) -> {next_state, NextStateName, NextState}|
-%%                             {next_state, NextStateName,
-%%                                NextState, Timeout} |
-%%                             {stop, Reason, NewState}
-%% Description:There should be one instance of this function for each possible
-%% state name. Whenever a gen_fsm receives an event sent using
-%% gen_fsm:send_event/2, the instance of this function with the same name as
-%% the current state name StateName is called to handle the event. It is also
-%% called if a timeout occurs.
+%% Function: init(Args) -> {ok, State} |
+%%                         {ok, State, Timeout} |
+%%                         ignore               |
+%%                         {stop, Reason}
+%% Description: Initiates the server
 %%--------------------------------------------------------------------
-initial(connect, S) ->
-    io:format("Trying to connect to ~p~n", [S]),
-    {next_state, initial, S};
-initial(_Event, S) ->
-    {next_state, initial, S}.
+init([IP, Port, PeerId, InfoHash]) ->
+    {ok, #state{ ip = IP,
+		 port = Port,
+		 peer_id = PeerId,
+		 info_hash = InfoHash}}.
 
 %%--------------------------------------------------------------------
-%% Function:
-%% state_name(Event, From, State) -> {next_state, NextStateName, NextState} |
-%%                                   {next_state, NextStateName,
-%%                                     NextState, Timeout} |
-%%                                   {reply, Reply, NextStateName, NextState}|
-%%                                   {reply, Reply, NextStateName,
-%%                                    NextState, Timeout} |
-%%                                   {stop, Reason, NewState}|
-%%                                   {stop, Reason, Reply, NewState}
-%% Description: There should be one instance of this function for each
-%% possible state name. Whenever a gen_fsm receives an event sent using
-%% gen_fsm:sync_send_event/2,3, the instance of this function with the same
-%% name as the current state name StateName is called to handle the event.
+%% Function: %% handle_call(Request, From, State) -> {reply, Reply, State} |
+%%                                      {reply, Reply, State, Timeout} |
+%%                                      {noreply, State} |
+%%                                      {noreply, State, Timeout} |
+%%                                      {stop, Reason, Reply, State} |
+%%                                      {stop, Reason, State}
+%% Description: Handling call messages
 %%--------------------------------------------------------------------
-initial(_E, _F, S) ->
-    {reply, ok, initial, S}.
-
-%%--------------------------------------------------------------------
-%% Function:
-%% handle_event(Event, StateName, State) -> {next_state, NextStateName,
-%%						  NextState} |
-%%                                          {next_state, NextStateName,
-%%					          NextState, Timeout} |
-%%                                          {stop, Reason, NewState}
-%% Description: Whenever a gen_fsm receives an event sent using
-%% gen_fsm:send_all_state_event/2, this function is called to handle
-%% the event.
-%%--------------------------------------------------------------------
-handle_event(_Event, StateName, State) ->
-    {next_state, StateName, State}.
-
-%%--------------------------------------------------------------------
-%% Function:
-%% handle_sync_event(Event, From, StateName,
-%%                   State) -> {next_state, NextStateName, NextState} |
-%%                             {next_state, NextStateName, NextState,
-%%                              Timeout} |
-%%                             {reply, Reply, NextStateName, NextState}|
-%%                             {reply, Reply, NextStateName, NextState,
-%%                              Timeout} |
-%%                             {stop, Reason, NewState} |
-%%                             {stop, Reason, Reply, NewState}
-%% Description: Whenever a gen_fsm receives an event sent using
-%% gen_fsm:sync_send_all_state_event/2,3, this function is called to handle
-%% the event.
-%%--------------------------------------------------------------------
-handle_sync_event(_Event, _From, StateName, State) ->
+handle_call(_Request, _From, State) ->
     Reply = ok,
-    {reply, Reply, StateName, State}.
+    {reply, Reply, State}.
 
 %%--------------------------------------------------------------------
-%% Function:
-%% handle_info(Info,StateName,State)-> {next_state, NextStateName, NextState}|
-%%                                     {next_state, NextStateName, NextState,
-%%                                       Timeout} |
-%%                                     {stop, Reason, NewState}
-%% Description: This function is called by a gen_fsm when it receives any
-%% other message than a synchronous or asynchronous event
-%% (or a system message).
+%% Function: handle_cast(Msg, State) -> {noreply, State} |
+%%                                      {noreply, State, Timeout} |
+%%                                      {stop, Reason, State}
+%% Description: Handling cast messages
 %%--------------------------------------------------------------------
-handle_info(_Info, StateName, State) ->
-    {next_state, StateName, State}.
+handle_cast({connect, MyPeerId}, S) ->
+    Socket = int_connect(S#state.ip, S#state.port),
+    % TODO: use initiate_handshake/4
+    case peer_communication:initiate_handshake(Socket,
+					       S#state.peer_id,
+					       MyPeerId,
+					       S#state.info_hash) of
+	{ok, _ReservedBytes} ->
+	    enable_socket_messages(Socket),
+	    {noreply, S#state{tcp_socket = Socket}};
+	{error, X} ->
+	    exit(X)
+    end;
+handle_cast(_Msg, State) ->
+    {noreply, State}.
 
 %%--------------------------------------------------------------------
-%% Function: terminate(Reason, StateName, State) -> void()
-%% Description:This function is called by a gen_fsm when it is about
-%% to terminate. It should be the opposite of Module:init/1 and do any
-%% necessary cleaning up. When it returns, the gen_fsm terminates with
-%% Reason. The return value is ignored.
+%% Function: handle_info(Info, State) -> {noreply, State} |
+%%                                       {noreply, State, Timeout} |
+%%                                       {stop, Reason, State}
+%% Description: Handling all non call/cast messages
 %%--------------------------------------------------------------------
-terminate(_Reason, _StateName, _State) ->
+handle_info({tcp_closed, _P}, S) ->
+    {stop, normal, S};
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% Function: terminate(Reason, State) -> void()
+%% Description: This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any necessary
+%% cleaning up. When it returns, the gen_server terminates with Reason.
+%% The return value is ignored.
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
     ok.
 
 %%--------------------------------------------------------------------
-%% Function:
-%% code_change(OldVsn, StateName, State, Extra) -> {ok, StateName, NewState}
+%% Func: code_change(OldVsn, State, Extra) -> {ok, NewState}
 %% Description: Convert process state when code is changed
 %%--------------------------------------------------------------------
-code_change(_OldVsn, StateName, State, _Extra) ->
-    {ok, StateName, State}.
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
 
 %%--------------------------------------------------------------------
 %%% Internal functions
 %%--------------------------------------------------------------------
+
+% Specialize connects to our purpose
+int_connect(IP, Port) ->
+    case gen_tcp:connect(IP, Port, [binary, {active, false}],
+			 ?DEFAULT_CONNECT_TIMEOUT) of
+	{ok, Socket} ->
+	    Socket;
+	{error, _Reason} ->
+	    exit(normal)
+    end.
+
+enable_socket_messages(Socket) ->
+    inet:setopts(Socket, [binary, {active, true}, {packet, 4}]).
