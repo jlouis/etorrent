f5aa6b424fc54610d0af656e05e3aba1113f9eba
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2007-07-24 15:52:38 +0200

    Set the port env variable in tests.

--- lib/etorrent-1.0/src/test.erl
+++ lib/etorrent-1.0/src/test.erl
@@ -8,5 +8,6 @@ start() ->
     timer:start(),
     %http:set_options([{verbose, debug}]),
     application:set_env(etorrent, dir, "/home/jlouis/etorrent_test"),
+    application:set_env(etorrent, port, 1729),
     error_logger:logfile({open, "err.log"}),
     etorrent:start_link().
