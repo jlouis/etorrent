bf192d298f7ae8b56f38545c9738bc719685e899
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2007-08-07 23:55:54 +0200

    http_gzip wraps the inets applications http:request/1 to provide gzip support.

---
+++ lib/etorrent-1.0/src/http_gzip.erl
@@ -0,0 +1,58 @@
+%%%-------------------------------------------------------------------
+%%% File    : inets_gzip.erl
+%%% Author  : Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
+%%% Description : Wrapper around http to honor gzip.
+%%%
+%%% Created :  7 Aug 2007 by Jesper Louis Andersen <>
+%%%-------------------------------------------------------------------
+-module(http_gzip).
+
+%% API
+-export([request/1]).
+
+%%====================================================================
+%% API
+%%====================================================================
+%%--------------------------------------------------------------------
+%% Function: request/1
+%% Description: As http:request/1 in the inets application, but also
+%%   handles gzip.
+%%--------------------------------------------------------------------
+request(URL) ->
+    case http:request(get, {URL, [{"Accept-Encoding", "gzip"}]}, [], []) of
+	{ok, {{Version, StatusCode, ReasonPhrase}, Headers, Body}} ->
+	    case decode_content_encoding(Headers) of
+		identity ->
+		    error_logger:info_report([identity]),
+		    {ok, {{Version, StatusCode, ReasonPhrase}, Headers, Body}};
+		gzip ->
+		    error_logger:info_report([gzip]),
+		    DecompressedBody = binary_to_list(zlib:gunzip(Body)),
+		    {ok, {{Version, StatusCode, ReasonPhrase}, Headers,
+		     DecompressedBody}}
+	    end;
+	E ->
+	    E
+    end.
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
+decode_content_encoding(Headers) ->
+    LowerCaseHeaderKeys = lists:map(fun({K, V}) ->
+					    {string:to_lower(K), V}
+				    end,
+				    Headers),
+    case lists:keysearch("content-encoding", 1, LowerCaseHeaderKeys) of
+	{value, {_, "gzip"}} ->
+	    gzip;
+	{value, {_, "deflate"}} ->
+	    deflate;
+	{value, {_, "identity"}} ->
+	    identity;
+	false ->
+	    identity
+    end.
+
+	
+	    
