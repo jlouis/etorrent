712ee647fa74f61605b65bb1a22d95a637880ed3
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2007-08-17 22:37:33 +0200

    Implement support for peer processes.

--- lib/etorrent-1.0/src/etorrent_t_peer_pool_sup.erl
+++ lib/etorrent-1.0/src/etorrent_t_peer_pool_sup.erl
@@ -25,24 +25,20 @@
 %% Description: Starts the supervisor
 %%--------------------------------------------------------------------
 start_link() ->
-    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+    supervisor:start_link(?MODULE, []).
+
+add_peer(GroupPid, LocalPeerId, InfoHash, StatePid, FilesystemPid, Parent) ->
+    supervisor:start_child(GroupPid, [LocalPeerId, InfoHash, StatePid,
+				     FilesystemPid, Parent]).
 
 %%====================================================================
 %% Supervisor callbacks
 %%====================================================================
-%%--------------------------------------------------------------------
-%% Func: init(Args) -> {ok,  {SupFlags,  [ChildSpec]}} |
-%%                     ignore                          |
-%%                     {error, Reason}
-%% Description: Whenever a supervisor is started using 
-%% supervisor:start_link/[2,3], this function is called by the new process 
-%% to find out about restart strategy, maximum restart frequency and child 
-%% specifications.
-%%--------------------------------------------------------------------
 init([]) ->
-    AChild = {'AName',{'AModule',start_link,[]},
-	      permanent,2000,worker,['AModule']},
-    {ok,{{one_for_all,0,1}, [AChild]}}.
+    PeerRecvs = {peer_recv,
+		 {etorrent_t_peer_recv, start_link, []},
+		 transient, infinity, supervisor, [etorrent_t_peer_recv]},
+    {ok, {{simple_one_for_one, 1, 60}, [PeerRecvs]}}.
 
 %%====================================================================
 %% Internal functions
