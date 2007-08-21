272f82169077d504916127cc83fba1afa68c5bc3
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2007-08-21 19:59:00 +0200

    Begin supervision of file processes. Start with the supervisor.

---
+++ lib/etorrent-1.0/src/etorrent_fs_pool_sup.erl
@@ -0,0 +1,48 @@
+%%%-------------------------------------------------------------------
+%%% File    : etorrent_fs_pool_sup.erl
+%%% Author  : Jesper Louis Andersen <>
+%%% Description : Supervise a set of file system processes.
+%%%
+%%% Created : 21 Aug 2007 by Jesper Louis Andersen <>
+%%%-------------------------------------------------------------------
+-module(etorrent_fs_pool_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0, add_file_process/2]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+%%====================================================================
+%% API functions
+%%====================================================================
+%%--------------------------------------------------------------------
+%% Function: start_link() -> {ok,Pid} | ignore | {error,Error}
+%% Description: Starts the supervisor
+%%--------------------------------------------------------------------
+start_link() ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+%%--------------------------------------------------------------------
+%% Function: add_file_process/2
+%% Description: Add a new process for maintaining a file.
+%%--------------------------------------------------------------------
+add_file_process(Pid, Path) ->
+    supervisor:start_child(Pid, [Path]).
+
+%%====================================================================
+%% Supervisor callbacks
+%%====================================================================
+init([]) ->
+    FSProcesses = {'FSPROCESS',
+		   {etorrent_fs_process, start_link, []},
+		   transients, 2000, worker, [etorrent_fs_process]},
+    {ok, {{simple_one_for_one, 1, 60}, [FSProcesses]}}.
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
