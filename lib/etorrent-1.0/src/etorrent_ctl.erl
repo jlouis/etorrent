9db17585927226fefacb50d6d37bd1dfeb91b9e5
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2008-07-03 18:48:28 +0200

    Preliminary etorrent_ctl and manipulation code for it.

---
+++ lib/etorrent-1.0/src/etorrent_ctl.erl
@@ -0,0 +1,51 @@
+%%%-------------------------------------------------------------------
+%%% File    : etorrent_ctl.erl
+%%% Author  : Jesper Louis Andersen <>
+%%% Description : Controller. Called from a distributed node towards etorrent
+%%%
+%%% Created :  3 Jul 2008 by Jesper Louis Andersen <>
+%%%-------------------------------------------------------------------
+-module(etorrent_ctl).
+
+%% API
+-export([start/1, process/1]).
+
+-define(STATUS_OK, 0).
+-define(STATUS_BADRPC, 1).
+-define(STATUS_USAGE, 2).
+
+%%====================================================================
+%% API
+%%====================================================================
+%%--------------------------------------------------------------------
+%% Function: start() -> ok
+%% Description: This is the command which is run to control etorrent
+%%--------------------------------------------------------------------
+start (Args) ->
+    case parse_arguments(Args) of
+	{Node, Commands} ->
+	    Status = case rpc:call(Node, ?MODULE, process, [Commands]) of
+			 {badprc, Reason} ->
+			     io:format("RPC failed on node ~p: ~p~n",
+				       [Node, Reason]),
+			     ?STATUS_BADRPC;
+			 S ->
+			     S
+		     end,
+	    halt(Status);
+	_ ->
+	    halt(?STATUS_USAGE)
+    end.
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
+process(["stop"]) ->
+    etorrent:stop(),
+    ?STATUS_OK.
+
+parse_arguments([Node | Cmds]) ->
+    {list_to_atom(Node),
+     Cmds}.
+
+    
