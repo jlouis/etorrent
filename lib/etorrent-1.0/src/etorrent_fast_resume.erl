ce03ab1de2b075a390f0075ee4a9084350eb0fb7
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2010-05-20 23:14:16 +0200

    Fix a bug in the fast resume system.
    When we were a looong time at checking in the startup phase, I had made
    the mistake of introducing a badmatch by matching
    
    {_, _} = ok
    
    essentially. This of course never works. Rewrote code to a simpler form
    which ought to work in any case.

--- lib/etorrent-1.0/src/etorrent_fast_resume.erl
+++ lib/etorrent-1.0/src/etorrent_fast_resume.erl
@@ -20,13 +20,6 @@
 
 -record(state, { timer = none }).
 
-%% Type of the state in the diskstate in question.
--type(diskstate_state() :: 'seeding' | {'bitfield', binary()}).
-
-%% Piece state on disk for persistence
--record(piece_diskstate, {filename :: string(), % Name of torrent
-                          state :: diskstate_state()}).
-
 -define(SERVER, ?MODULE).
 -define(PERSIST_TIME, timer:seconds(300)). % Every 300 secs, may be done configurable.
 
@@ -48,17 +41,33 @@ start_link() ->
 query_state(Id) ->
         gen_server:call(?SERVER, {query_state, Id}).
 
-%%====================================================================
-%% gen_server callbacks
-%%====================================================================
+%% ==================================================================
 
-%%--------------------------------------------------------------------
-%% Function: init(Args) -> {ok, State} |
-%%                         {ok, State, Timeout} |
-%%                         ignore               |
-%%                         {stop, Reason}
-%% Description: Initiates the server
-%%--------------------------------------------------------------------
+%% Enter a torrent into the tracking table
+track_torrent(#tracking_map { id = Id, filename = FName}) ->
+    case etorrent_torrent:state(Id) of
+        not_found -> ignore;
+        {value, unknown} -> ignore;
+        {value, seeding} -> ets:insert(?MODULE, {FName, seeding});
+        {value, _Other}  -> ets:insert(?MODULE,
+                                {FName, {bitfield, etorrent_piece_mgr:bitfield(Id)}})
+    end.
+
+%% Enter all torrents into a tracking table
+track_in_ets_table(Lst) when is_list(Lst) ->
+    [track_torrent(TM) || TM <- Lst].
+
+%% Run a persistence operation
+persist_to_disk() ->
+    {atomic, Torrents} = etorrent_tracking_map:all(),
+    _ = track_in_ets_table(Torrents),
+    {ok, F} = application:get_env(etorrent, fast_resume_file),
+    ok = ets:tab2file(etorrent_fast_resume, F, [{extended_info, [object_count, md5sum]}]),
+    etorrent_event_mgr:persisted_state_to_disk(),
+    ok.
+
+%% ==================================================================
+
 init([]) ->
     process_flag(trap_exit, true),
     {ok, F} = application:get_env(etorrent, fast_resume_file),
@@ -72,40 +81,19 @@ init([]) ->
     {ok, TRef} = timer:send_interval(?PERSIST_TIME, self(), persist),
     {ok, #state{ timer = TRef}}.
 
-%%--------------------------------------------------------------------
-%% Function: %% handle_call(Request, From, State) -> {reply, Reply, State} |
-%%                                      {reply, Reply, State, Timeout} |
-%%                                      {noreply, State} |
-%%                                      {noreply, State, Timeout} |
-%%                                      {stop, Reason, Reply, State} |
-%%                                      {stop, Reason, State}
-%% Description: Handling call messages
-%%--------------------------------------------------------------------
 handle_call({query_state, Id}, _From, S) ->
     {atomic, [TM]} = etorrent_tracking_map:select(Id),
     case ets:lookup(etorrent_fast_resume, TM#tracking_map.filename) of
         [] -> {reply, unknown, S};
-        [{_, R}] -> {reply, R#piece_diskstate.state, S}
+        [{_, R}] -> {reply, R, S}
     end;
 handle_call(_Request, _From, State) ->
     Reply = ok,
     {reply, Reply, State}.
 
-%%--------------------------------------------------------------------
-%% Function: handle_cast(Msg, State) -> {noreply, State} |
-%%                                      {noreply, State, Timeout} |
-%%                                      {stop, Reason, State}
-%% Description: Handling cast messages
-%%--------------------------------------------------------------------
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
-%%--------------------------------------------------------------------
-%% Function: handle_info(Info, State) -> {noreply, State} |
-%%                                       {noreply, State, Timeout} |
-%%                                       {stop, Reason, State}
-%% Description: Handling all non call/cast messages
-%%--------------------------------------------------------------------
 handle_info(persist, S) ->
     error_logger:info_report([persist_to_disk]),
     persist_to_disk(),
@@ -114,61 +102,11 @@ handle_info(_Info, State) ->
     {noreply, State}.
 
 
-%%--------------------------------------------------------------------
-%% Function: terminate(Reason, State) -> void()
-%% Description: This function is called by a gen_server when it is about to
-%% terminate. It should be the opposite of Module:init/1 and do any necessary
-%% cleaning up. When it returns, the gen_server terminates with Reason.
-%% The return value is ignored.
-%%--------------------------------------------------------------------
 terminate(normal, _State) ->
     persist_to_disk(),
     ok;
 terminate(_Reason, _State) ->
     ok.
 
-
-%%--------------------------------------------------------------------
-%% Func: code_change(OldVsn, State, Extra) -> {ok, NewState}
-%% Description: Convert process state when code is changed
-%%--------------------------------------------------------------------
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
-
-%%--------------------------------------------------------------------
-%%% Internal functions
-%%--------------------------------------------------------------------
-
-%%--------------------------------------------------------------------
-%% Func: prune_disk_state(Tracking) -> ok
-%% Description: Prune the persistent disk state for anything we are
-%%   not tracking.
-%%--------------------------------------------------------------------
-
-%%--------------------------------------------------------------------
-%% Func: persist_disk_state(Tracking) -> ok
-%% Description: Persist state on disk
-%%--------------------------------------------------------------------
-track_in_ets_table([]) -> ok;
-track_in_ets_table([#tracking_map { id = Id,
-                                    filename = FName} | Next]) ->
-    {F, St} = case etorrent_torrent:state(Id) of
-        not_found -> track_in_ets_table(Next); %% Not hot, skip it.
-        {value, seeding}  -> {FName, seeding};
-        {value, leeching} -> {FName, {bitfield, etorrent_piece_mgr:bitfield(Id)}};
-        {value, endgame}  -> {FName, {bitfield, etorrent_piece_mgr:bitfield(Id)}};
-        {value, unknown}  -> track_in_ets_table(Next) %% Skip, we know nothing yet
-    end,
-    true = ets:insert(etorrent_fast_resume, #piece_diskstate{filename = F,
-                                                             state    = St}),
-    track_in_ets_table(Next).
-
-persist_to_disk() ->
-    {atomic, Torrents} = etorrent_tracking_map:all(),
-    track_in_ets_table(Torrents),
-    {ok, F} = application:get_env(etorrent, fast_resume_file),
-    ok = ets:tab2file(etorrent_fast_resume, F, [{extended_info, [object_count, md5sum]}]),
-    etorrent_event_mgr:persisted_state_to_disk(),
-    ok.
-
-
