2d4e2ab146455559293331c4335dced8ec6b03dc
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2010-06-12 23:51:57 +0200

    Improvementificate documentation in etorrent.erl.

--- src/etorrent.erl
+++ src/etorrent.erl
@@ -8,11 +8,18 @@
 -export([stop/0, start/0]).
 -export([start/2, stop/1, prep_stop/1]).
 -export([help/0, h/0, list/0, l/0, show/0, s/0, show/1, s/1, check/1]).
+
 -ignore_xref([{h, 0}, {l, 0}, {'prep_stop', 1}, {s, 0}, {s,1}, {stop, 0},
               {check, 1}, {start, 0}]).
 
 -define(RANDOM_MAX_SIZE, 999999999999).
 
+%% @doc Start up the etorrent application.
+%% <p>This is the main entry point for the etorrent application.
+%% It will be ensured that needed apps are correctly up and running,
+%% and then the etorrent application will be started as a permanent app
+%% in the VM.</p>
+%% @end
 start() ->
     %% Start dependent applications
     Fun = fun(Application) ->
@@ -29,25 +36,36 @@ start() ->
     %% Etorrent
     application:start(etorrent, permanent).
 
+%% @doc Application callback.
+%% @end
 start(_Type, _Args) ->
     PeerId = generate_peer_id(),
     {ok, Pid} = etorrent_sup:start_link(PeerId),
     {ok, Pid}.
 
+%% @doc Application callback.
+%% @end
 stop() ->
     ok = application:stop(etorrent).
 
+%% @doc Application callback.
+%% @end
 prep_stop(_S) ->
     io:format("Shutting down etorrent~n"),
     ok.
 
+%% @doc Application callback.
+%% @end
 stop(_State) ->
     ok.
 
-%%--------------------------------------------------------------------
-%% Function: list() -> io()
-%% Description: List currently active torrents.
-%%--------------------------------------------------------------------
+%% @doc List currently active torrents.
+%% <p>This function will list the torrent files which are currently in
+%% the active state in the etorrent system. A general breakdown of each
+%% torrent and its current states is given. The function is given as a
+%% convenience in the shell while the system is running.</p>
+%% @end
+-spec list() -> ok.
 list() ->
     A = etorrent_torrent:all(),
     {DownloadRate, UploadRate} = etorrent_rate_mgr:global_rate(),
@@ -77,13 +95,13 @@ list() ->
     io:format("Rate Up/Down: ~8.2f / ~8.2f~n", [UploadRate / 1024.0,
                                                 DownloadRate / 1024.0]).
 
-%%--------------------------------------------------------------------
-%% Function: show(Item) -> io()
-%% Description: Show detailed information for Item
-%%--------------------------------------------------------------------
+%% @doc Show detailed information for Item.
+%% @end
 show() ->
     io:format("You must supply a torrent Id number~n").
 
+%% @doc Show detailed information for Item.
+%% @end
 show(Item) when is_integer(Item) ->
     %{atomic, Torrent} = etorrent_torrent:select(Item),
     case etorrent_tracking_map:select(Item) of
@@ -96,17 +114,15 @@ show(Item) when is_integer(Item) ->
 show(_) ->
     io:format("Item supplied is not an integer~n").
 
-%%--------------------------------------------------------------------
-%% Function: check(Item) -> io()
-%% Description: Check a torrents contents. For debugging.
-%%--------------------------------------------------------------------
+%% @doc Check a torrents contents. For debugging.
+%% @end
 check(Id) ->
     etorrent_mgr:check(Id).
 
-%%--------------------------------------------------------------------
-%% Function: help() -> io()
-%% Description: Provide a simple help message for the commands supported.
-%%--------------------------------------------------------------------
+%% @doc Provide a simple help message for the commands supported.
+%% <p>This function will output a simple help message for the usage of the
+%% CLI to etorrent.</p>
+%% @end
 help() ->
     io:format("Available commands:~n", []),
 
@@ -122,20 +138,18 @@ help() ->
     ok.
 
 %%--------------------------------------------------------------------
-%% Abbreviations
-%%--------------------------------------------------------------------
 h() -> help().
 l() -> list().
 s() -> show().
 s(Item) -> show(Item).
 
-%% --------------------------------------------------------------------
-%% Internal functions
-%% --------------------------------------------------------------------
+%%=====================================================================
 percent_complete(R) ->
     %% left / complete * 100 = % done
     (R#torrent.total - R#torrent.left) / R#torrent.total * 100.
 
+%% @doc Generate a random peer id for use
+%% @end
 generate_peer_id() ->
     Number = crypto:rand_uniform(0, ?RANDOM_MAX_SIZE),
     Rand = io_lib:fwrite("~B----------", [Number]),
