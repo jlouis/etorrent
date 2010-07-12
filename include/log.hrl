6a92dad63a70320e6a3587047bac6963f70c30d4
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2010-07-12 23:12:10 +0200

    Suppress function and lineno in ?INFO log lines.

--- include/log.hrl
+++ include/log.hrl
@@ -1,5 +1,4 @@
--define(INFO(T), error_logger:info_report(
-        [process_info(self(), current_function), {line, ?LINE} | T])).
+-define(INFO(T), error_logger:info_report(T)).
 -define(WARN(T), error_logger:warning_report(
         [process_info(self(), current_function), {line, ?LINE} | T])).
 -define(ERR(T), error_logger:error_report(
