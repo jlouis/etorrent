a5668c99084a5efba7e4224353b734510bd22719
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2010-07-18 17:16:11 +0200

    Bump the protocol version.

--- include/etorrent_version.hrl
+++ include/etorrent_version.hrl
@@ -1,2 +1,5 @@
--define(VERSION, "d010"). %% Protocol version, really
--define(AGENT_TRACKER_STRING, "etorrent/1.0").
+%% Protocol version. It does not in general follow the versioning of the client itself.
+%% Rather the idea is that it is bumped whenever we fix something really grave that
+%% might get the client banned from communicating with other clients.
+-define(VERSION, "d011").
+-define(AGENT_TRACKER_STRING, "etorrent/1.0.0").
