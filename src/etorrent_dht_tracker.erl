5dc10b9877f552b3cf71179e68712a53c2434712
klaar <klaar@cohen.(none)>
2010-09-28 02:28:59 +0200

    decreased number of peers allowed per swarm in tracker and simplified handling of that limit

--- src/etorrent_dht_tracker.erl
+++ src/etorrent_dht_tracker.erl
@@ -7,6 +7,7 @@
 tab_name() ->
     etorrent_dht_tracker_tab.
 
+
 init() ->
     case ets:info(tab_name()) of
         undefined ->
@@ -16,29 +17,34 @@ init() ->
             ok
     end.
 
+%
+% Register a peer as a member of a swarm. If the maximum number of
+% registered peers for a torrent is exceeded, one or more peers are
+% deleted.
+%
 announce(InfoHash, IP, Port) ->
-    % Limit the amount of peers associated to a torrent to 80 entries.
-    MSpec = [{{{peer,InfoHash},'$1','$2'}, [], [{{{{peer,InfoHash}},'$1','$2'}}]}],
-    case ets:select(tab_name(), MSpec, 79) of
-        '$end_of_table' ->
-            announce(InfoHash, IP, Port, '$end_of_table');
-        {_, Cont} ->
-            announce(InfoHash, IP, Port, Cont)
-    end.
+    % Always delete a random peer when inserting a new
+    % peer into the table. If limit is not reached yet, the
+    % chances of deleting a peer for no good reason are lower.
+    RandPeer = random_peer(),
+    DelSpec  = [{{InfoHash,'$1','$2',RandPeer},[],[true]}],
+    _ = ets:select_delete(tab_name(), DelSpec),
+    ets:insert(tab_name(), {InfoHash, IP, Port, RandPeer}).
+       
+   
 
-announce(InfoHash, IP, Port, '$end_of_table') ->
-    Key = {peer, InfoHash},
-    Obj = {Key, IP, Port},
-    ets:insert(tab_name(), Obj);
-
-announce(InfoHash, IP, Port, Continuation) ->
-    {Peers, NextCont} = ets:select(Continuation),
-    _ = [ets:delete_object(tab_name(), P) || P <- Peers],
-    announce(InfoHash, IP, Port, NextCont).
-
-
+%
+% Get the peers that are registered as members of this swarm.
+%
 get_peers(InfoHash) ->
-    case ets:select(tab_name(), [{{{peer,InfoHash},'$1','$2'},[],[{{'$1','$2'}}]}], 80) of
+    GetSpec = [{{InfoHash,'$1','$2','_'},[],[{{'$1','$2'}}]}],
+    case ets:select(tab_name(), GetSpec, max_per_torrent()) of
         {PeerInfos, _} -> PeerInfos;
         '$end_of_table' -> []
     end.
+
+max_per_torrent() ->
+    32.
+
+random_peer() ->
+    random:uniform(max_per_torrent()).
