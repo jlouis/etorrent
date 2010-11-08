a11e047f16a2deb0eb6c0e04ffb45dd2ca7d0669
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2010-11-08 16:04:44 +0100

    Make the webui be started dynamically by etorrent.
    This change embeds the webui configuration directly inside etorrent,
    rather than relying on a separate configuration file. There is a
    regression since we now need to add specifics of binding to a given
    port to the code, but otherwise it seem to work.
    
    While here, move log files and private data to the right spots by
    utilizing the Erlang/OTP code and application modules.

--- apps/etorrent/src/etorrent_app.erl
+++ apps/etorrent/src/etorrent_app.erl
@@ -17,6 +17,12 @@ start(_Type, _Args) ->
     %% DB
     etorrent_mnesia_init:init(),
     etorrent_mnesia_init:wait(),
+    case application:get_env(etorrent, webui) of
+	{ok, true} ->
+	    start_webui();
+	{ok, false} ->
+	    ignore
+    end,
     etorrent_sup:start_link(PeerId).
 
 %% @doc Application callback.
@@ -29,6 +35,40 @@ prep_stop(_S) ->
 %% @end
 stop(_State) ->
     ok.
+
+start_webui() ->
+    Config = default_webui_configuration(),
+    inets:start(httpd, Config, inets).
+
+logger_dir() ->
+    {ok, Val} = application:get_env(etorrent, logger_dir),
+    Val.
+
+default_webui_configuration() ->
+    [{modules,
+      [mod_alias,
+       mod_auth,
+       mod_esi,
+       mod_actions,
+       mod_cgi,
+       mod_dir,
+       mod_get,
+       mod_head,
+       mod_log,
+       mod_disk_log]},
+     {mime_types, [{"html", "text/html"},
+		   {"css", "text/css"},
+		   {"js", "text/javascript"}]},
+     {server_name,"etorrent_webui"},
+     {bind_address, {127,0,0,1}},
+     {server_root, filename:join([logger_dir(), "webui"])},
+     {port,8080},
+     {document_root, filename:join([code:priv_dir(etorrent), "webui", "htdocs"])},
+     {directory_index, ["index.html"]},
+     {erl_script_alias, {"/ajax", [etorrent_webui]}},
+     {error_log, "error.log"},
+     {security_log, "security.log"},
+     {transfer_log, "transfer.log"}].
 
 %% @doc Generate a random peer id for use
 %% @end
