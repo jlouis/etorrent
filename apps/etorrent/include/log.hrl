908b9e95673363c5e7f4c6c8909df0ed15b54da2
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2010-11-23 18:52:47 +0100

    Debugging helpers added.

--- apps/etorrent/include/log.hrl
+++ apps/etorrent/include/log.hrl
@@ -3,3 +3,9 @@
         [process_info(self(), current_function), {line, ?LINE} | T])).
 -define(ERR(T), error_logger:error_report(
         [process_info(self(), current_function), {line, ?LINE} | T])).
+
+-define(DEBUG(Format, Args), io:format("D(~p:~p:~p) : "++Format++"~n",
+                                       [self(),?MODULE,?LINE]++Args)).
+-define(DEBUGP(Args), io:format("D(~p:~p:~p) : ~p~n",
+                                       [self(),?MODULE,?LINE, Args])).
+
