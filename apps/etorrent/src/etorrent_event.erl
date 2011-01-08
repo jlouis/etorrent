39763d5f776dc2d47de8c0418430f9d3c35d6ead
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2011-01-08 11:30:07 +0100

    Document the event code.

--- apps/etorrent/src/etorrent_event.erl
+++ apps/etorrent/src/etorrent_event.erl
@@ -1,10 +1,11 @@
-%%%-------------------------------------------------------------------
-%%% File    : etorrent_event.erl
-%%% Author  : Jesper Louis Andersen <>
-%%% Description : Event tracking inside the etorrent application.
-%%%
-%%% Created : 25 Aug 2007 by Jesper Louis Andersen <>
-%%%-------------------------------------------------------------------
+%% @author Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
+%% @doc Handle messaging events in etorrent.
+%% <p>This module handles the processing of <em>event messages</em> in
+%% etorrent. Whenever something important happens, a message is sent
+%% to then `gen_event' process. It then hands the message on to any
+%% handler which has been registered with it. Normally these are
+%% handlers for file logging and memory logging.</p>
+%% @end
 -module(etorrent_event).
 
 %% Installation/deinstallation of the event mgr
@@ -22,30 +23,45 @@
 -define(SERVER, ?MODULE).
 -ignore_xref([{start_link, 0}, {event, 1}]).
 %% =======================================================================
+
+%% @doc Notify the event system of an event
+%% <p>The system accepts any term as the event.</p>
+%% @end
 -spec notify(term()) -> ok.
 notify(What) ->
     gen_event:notify(?SERVER, What).
 
+%% @doc Add an event handler
+%% @end
 -spec add_handler(atom() | pid(), [term()]) -> ok.
 add_handler(Handler, Args) ->
     gen_event:add_handler(?SERVER, Handler, Args).
 
+%% @doc Delete an event handler
+%% @end
 -spec delete_handler(atom() | pid(), [term()]) -> ok.
 delete_handler(Handler, Args) ->
     gen_event:delete_handler(?SERVER, Handler, Args).
 
+%% @equiv notify({started_torrent, Id})
 -spec started_torrent(integer()) -> ok.
 started_torrent(Id) -> notify({started_torrent, Id}).
 
+%% @equiv notify({checking_torrent, Id})
 -spec checking_torrent(integer()) -> ok.
 checking_torrent(Id) -> notify({checking_torrent, Id}).
 
+%% @equiv notify({seeding_torrent, Id})
 -spec seeding_torrent(integer()) -> ok.
 seeding_torrent(Id) -> notify({seeding_torrent, Id}).
 
+%% @equiv notify({completed_torrent, Id})
 -spec completed_torrent(integer()) -> ok.
 completed_torrent(Id) -> notify({completed_torrent, Id}).
 
 %% ====================================================================
+
+%% @doc Start the event handler
+%% @end
 start_link() ->
     gen_event:start_link({local, ?SERVER}).
