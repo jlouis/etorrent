e0533be79877eedce050fa2f36df4dccb269f225
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2011-01-13 11:50:37 +0100

    Document the etorrent_io_sup code.

--- apps/etorrent/src/etorrent_io_sup.erl
+++ apps/etorrent/src/etorrent_io_sup.erl
@@ -1,3 +1,8 @@
+%% @author Magnus Klaar <magnus.klaar@sgsstudentbostader.se>
+%% @doc Supervise a set of file system processes
+%% <p>This supervisor is responsible for overseeing the file system
+%% processes for a single torrent.</p>
+%% @end
 -module(etorrent_io_sup).
 -behaviour(supervisor).
 -include("types.hrl").
@@ -5,10 +10,17 @@
 -export([start_link/2]).
 -export([init/1]).
 
+%% @doc Initiate the supervisor.
+%% <p>The arguments are the ID of the torrent and the file-path at
+%% which the torrent file lives</p>
+%% @end
 -spec start_link(torrent_id(), file_path()) -> {'ok', pid()}.
 start_link(TorrentID, TorrentFile) ->
     supervisor:start_link(?MODULE, [TorrentID, TorrentFile]).
 
+%% ----------------------------------------------------------------------
+
+%% @private
 init([TorrentID, TorrentFile]) ->
     Workdir   = etorrent_config:work_dir(),
     FullPath  = filename:join([Workdir, TorrentFile]),
@@ -18,6 +30,7 @@ init([TorrentID, TorrentFile]) ->
     FileSup   = file_server_sup_spec(TorrentID, Workdir, Files),
     {ok, {{one_for_one, 1, 60}, [DirServer, FileSup]}}.
 
+%% ----------------------------------------------------------------------
 directory_server_spec(TorrentID, Torrent) ->
     {{TorrentID, directory},
         {etorrent_io, start_link, [TorrentID, Torrent]},
