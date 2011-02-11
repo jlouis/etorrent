eb57a60a48f4ba6197738f46a9c6493c877f606c
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2011-02-11 01:00:27 +0100

    Get rid of test we don't use.

--- test/etorrent_SUITE.erl
+++ test/etorrent_SUITE.erl
@@ -6,7 +6,7 @@
 	 init_per_suite/1, end_per_suite/1,
 	 init_per_testcase/2, end_per_testcase/2]).
 
--export([member/1, seed_leech/1]).
+-export([seed_leech/1]).
 
 -define(TESTFILE30M, "test_file_30M.random.torrent").
 
@@ -112,7 +112,7 @@ leech_configuration(PrivDir, DataDir) ->
 %% Tests
 %% ----------------------------------------------------------------------
 all() ->
-    [member, seed_leech].
+    [seed_leech].
 
 seed_leech(Config) ->
     {Ref, Pid} = {make_ref(), self()},
@@ -124,37 +124,6 @@ seed_leech(Config) ->
     after
 	120*1000 -> exit(timeout_error)
     end.
-
-member(Config) when is_list(Config) ->
-    ?line {'EXIT',{badarg,_}} = (catch lists:member(45, {a,b,c})),
-    ?line {'EXIT',{badarg,_}} = (catch lists:member(45, [0|non_list_tail])),
-    ?line false = lists:member(4233, []),
-    ?line member_test(1),
-    ?line member_test(100),
-    ?line member_test(256),
-    ?line member_test(1000),
-    ?line member_test(1998),
-    ?line member_test(1999),
-    ?line member_test(2000),
-    ?line member_test(2001),
-    ?line member_test(3998),
-    ?line member_test(3999),
-    ?line member_test(4000),
-    ?line member_test(4001),
-    ?line member_test(100008),
-    ok.
-member_test(Num) ->
-    List0 = ['The Element'|lists:duplicate(Num, 'Elem')],
-    true = lists:member('The Element', List0),
-    true = lists:member('Elem', List0),
-    false = lists:member(arne_anka, List0),
-    false = lists:member({a,b,c}, List0),
-    List = lists:reverse(List0),
-    true = lists:member('The Element', List),
-    true = lists:member('Elem', List),
-    false = lists:member(arne_anka, List),
-    false = lists:member({a,b,c}, List).
-
 
 %% Helpers
 %% ----------------------------------------------------------------------
