3b5cc654ec120c01d4fad7b7324a43d585fd0486
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2011-02-20 01:08:24 +0100

    QuickCheck the packet structure and fix bugs.

--- apps/utp/src/utp_proto.erl
+++ apps/utp/src/utp_proto.erl
@@ -2,6 +2,11 @@
 
 -include("utp.hrl").
 
+-ifdef(TEST).
+-include_lib("eqc/include/eqc.hrl").
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
 -export([mk_connection_id/0,
 	 send_packet/3,
 	 encode/3,
@@ -75,7 +80,7 @@ decode(Packet) ->
     end.
 
 decode_extensions(0, Payload, Exts) ->
-    {Exts, Payload};
+    {lists:reverse(Exts), Payload};
 decode_extensions(?EXT_SACK, <<Next:8/integer,
 			       Len:8/integer, R/binary>>, Acc) ->
     <<Bits:Len/binary, Rest/binary>> = R,
@@ -85,14 +90,15 @@ decode_extensions(?EXT_BITS, <<Next:8/integer,
     <<ExtBits:Len/binary, Rest/binary>> = R,
     decode_extensions(Next, Rest, [{ext_bits, ExtBits} | Acc]).
 
+encode_extensions([]) -> {0, <<>>};
 encode_extensions([{sack, Bits} | R]) ->
     {Next, Bin} = encode_extensions(R),
     Sz = byte_size(Bits),
-    {?EXT_SACK, <<Next:8/integer, Sz:8/integer, Bin/binary>>};
+    {?EXT_SACK, <<Next:8/integer, Sz:8/integer, Bits/binary, Bin/binary>>};
 encode_extensions([{ext_bits, Bits} | R]) ->
     {Next, Bin} = encode_extensions(R),
     Sz = byte_size(Bits),
-    {?EXT_BITS, <<Next:8/integer, Sz:8/integer, Bin/binary>>}.
+    {?EXT_BITS, <<Next:8/integer, Sz:8/integer, Bits/binary, Bin/binary>>}.
 
 decode_type(?ST_DATA) -> st_data;
 decode_type(?ST_FIN) -> st_fin;
@@ -106,13 +112,56 @@ encode_type(st_state) -> ?ST_STATE;
 encode_type(st_reset) -> ?ST_RESET;
 encode_type(st_syn) -> ?ST_SYN.
 
+-ifdef(EUNIT).
+-ifdef(EQC).
 
+g_type() ->
+    oneof([st_data, st_fin, st_state, st_reset, st_syn]).
 
+g_timestamp() ->
+    choose(0, 256*256*256*256-1).
 
+g_uint32() ->
+    choose(0, 256*256*256*256-1).
 
+g_uint16() ->
+    choose(0, 256*256-1).
 
+g_extension_one() ->
+    ?LET({What, Bin}, {oneof([sack, ext_bits]), binary()},
+	 {What, Bin}).
 
+g_extension() ->
+    list(g_extension_one()).
 
+g_packet() ->
+    ?LET({Ty, ConnID, WindowSize, SeqNo, AckNo,
+	  Extension, Payload},
+	 {g_type(), g_uint16(), g_uint32(), g_uint16(), g_uint16(),
+	  g_extension(), binary()},
+	 #packet { ty = Ty, conn_id = ConnID, win_sz = WindowSize,
+		   seq_no = SeqNo, ack_no = AckNo, extension = Extension,
+		   payload = Payload }).
 
+prop_ext_dec_inv() ->
+    ?FORALL(E, g_extension(),
+	    begin
+		{Next, B} = encode_extensions(E),
+		{E, <<>>} =:= decode_extensions(Next, B, [])
+	    end).
 
+prop_decode_inv() ->
+    ?FORALL({P, T1, T2}, {g_packet(), g_timestamp(), g_timestamp()},
+	    begin
+		{P, T1, T2} =:= decode(encode(P, T1, T2))
+	    end).
+
+inverse_extension_test() ->
+    ?assert(eqc:quickcheck(prop_ext_dec_inv())).
+
+inverse_decode_test() ->
+    ?assert(eqc:quickcheck(prop_decode_inv())).
+
+-endif.
+-endif.
 
