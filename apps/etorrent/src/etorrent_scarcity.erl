3f4eafd70dcd186b80e26490a0b80b83cf8fbbec
Magnus Klaar <magnus.klaar@sgsstudentbostader.se>
2011-03-03 20:33:02 +0100

    Add edoc strings for exported functions

--- apps/etorrent/src/etorrent_scarcity.erl
+++ apps/etorrent/src/etorrent_scarcity.erl
@@ -22,16 +22,22 @@
     
 
 %% @doc
-%%
+%% Return an indexed scarcity list based on the input. The input
+%% must be list of piece indexes paired with the numberof peers
+%% currently provide a valid copy of the piece.
 %% @end
 -spec from_list(list({pos_integer(), pos_integer()})) -> scarcity().
 from_list(ScarcityList) ->
+    [error(badarg) || {I,S} <- ScarcityList, (I < 0) or (S < 0)],
     InitCounters = array:from_orddict(ScarcityList),
     InitOrdering = gb_sets:from_list([{S,I} || {I,S} <- ScarcityList]),
     #scarcity{counters=InitCounters, ordering=InitOrdering}.
 
+
 %% @doc
-%%
+%% Returned a list of piece indexes ordered by how many peers are
+%% currently provide a valid copy of the piece. The returned list
+%% is sorted in increasing order.
 %% @end
 -spec to_list(#scarcity{}) -> list(pos_integer()).
 to_list(Scarcity) ->
@@ -40,20 +46,26 @@ to_list(Scarcity) ->
 
 
 %% @doc
-%%
+%% Insert a new piece with an initial number of providing peers into the
+%% scarcity list. The piece index must be larger than or equal to zero.
+%% It is up the calling process to check that the piece index is not higher
+%% than the number of pieces in the torrent.
 %% @end
 -spec insert(pos_integer(), pos_integer(), #scarcity{}) -> scarcity().
 insert(Index, Count, Scarcity) ->
     #scarcity{counters=Counters, ordering=Ordering} = Scarcity,
     assert_not_member(Index, Counters),
+    assert_positive(Count),
 
     NewCounters = array:set(Index, Count, Counters),
     NewOrdering = gb_sets:insert({Count, Index}, Ordering),
 
     #scarcity{counters=NewCounters, ordering=NewOrdering}.
 
+
 %% @doc
-%%
+%% Delete a piece from the scarcify. The piece must be a member of
+%% the scarcity list or the function will throw error(badarg).
 %% @end
 -spec delete(pos_integer(), #scarcity{}) -> scarcity().
 delete(Index, Scarcity) ->
@@ -66,21 +78,21 @@ delete(Index, Scarcity) ->
 
     #scarcity{counters=NewCounters, ordering=NewOrdering}.
 
-   
-
 
 %% @doc
-%%
+%% Increment the number of peers providing a piece by one and
+%% reprioritize the pieces in the scarcity list.
 %% @end
 -spec increment(pos_integer(), #scarcity{}) -> scarcity().
 increment(Index, Scarcity) ->
     #scarcity{counters=Counters, ordering=Ordering} = Scarcity,
     assert_member(Index, Counters),
 
-    Count = array:get(Index, Counters),
-    NewCounters = array:set(Index, Count + 1, Counters),
+    Count    = array:get(Index, Counters),
+    NewCount = Count + 1,
+    NewCounters = array:set(Index, NewCount, Counters),
     TmpOrdering = gb_sets:delete({Count, Index}, Ordering),
-    NewOrdering = gb_sets:insert({Count + 1, Index}, TmpOrdering),
+    NewOrdering = gb_sets:insert({NewCount, Index}, TmpOrdering),
 
     #scarcity{counters=NewCounters, ordering=NewOrdering}.
 
@@ -93,10 +105,12 @@ decrement(Index, Scarcity) ->
     #scarcity{counters=Counters, ordering=Ordering} = Scarcity,
     assert_member(Index, Counters),
 
-    Count = array:get(Index, Counters),
-    NewCounters = array:set(Index, Count - 1, Counters),
+    Count    = array:get(Index, Counters),
+    NewCount = Count - 1,
+    assert_positive(NewCount),
+    NewCounters = array:set(Index, NewCount, Counters),
     TmpOrdering = gb_sets:delete({Count, Index}, Ordering),
-    NewOrdering = gb_sets:insert({Count - 1, Index}, TmpOrdering),
+    NewOrdering = gb_sets:insert({NewCount, Index}, TmpOrdering),
 
     #scarcity{counters=NewCounters, ordering=NewOrdering}.
 
@@ -135,6 +149,7 @@ assert_member(Index, Counters) ->
             ok
     end.
 
+
 %% @doc
 %%
 %% @end
@@ -144,6 +159,19 @@ assert_not_member(Index, Counters) ->
         undefined ->
             ok;
         _ ->
+            error(badarg)
+    end.
+
+
+%% @doc
+%%
+%% @end
+-spec assert_positive(pos_integer()) -> ok.
+assert_positive(Integer) ->
+    case Integer < 0 of
+        false ->
+            ok;
+        true ->
             error(badarg)
     end.
 
@@ -172,7 +200,7 @@ increment_test() ->
     ?assertEqual([1,2,0], ?mod:to_list(S4)).
 
 decrement_test() ->
-    S0 = ?mod:from_list([{0,0}, {1,1}, {2,2}]),
+    S0 = ?mod:from_list([{0,0}, {1,1}, {2,4}]),
     S1 = ?mod:decrement(2, S0),
     S2 = ?mod:decrement(2, S1),
     S3 = ?mod:decrement(2, S2),
