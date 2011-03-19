9cd6f6b579b2a50f93eebe823614de40e7493dff
Magnus Klaar <magnus.klaar@sgsstudentbostader.se>
2011-03-19 15:29:47 +0100

    add docs and pipelining functions to rqueue module

--- apps/etorrent/src/etorrent_rqueue.erl
+++ apps/etorrent/src/etorrent_rqueue.erl
@@ -1,12 +1,30 @@
+%% @author Magnus Klaar <magnus.klaar@gmail.com>
+%% @doc Request queue for peers
+%% This module provides a wrapper for the queue module from stdlib, it
+%% assumes that responses to chunk requests are delivered in the same
+%% order as they were sent.
+%%
+%% The module provides additional functions for validating incoming
+%% requests against the head of the queue. We want to verify that the
+%% piece, offset and chunk length matches. We also want to be able to
+%% detect if the piece and offset but not the chunk length matches.
+%%
+%% The module also provides additional functions for checking if the
+%% request pipeline needs to be refilled or not. The thresholds are
+%% encapsulated in the request queue.
+%% @end
 -module(etorrent_rqueue).
 
 -export([new/0,
+         new/2,
          push/4,
          pop/1,
          peek/1,
          size/1,
          is_head/4,
-         has_offset/3]).
+         has_offset/3,
+         is_low/1,
+         needs/1]).
 
 
 -type pieceindex() :: etorrent_types:piece_index().
@@ -15,16 +33,29 @@
 -type requestspec() :: {pieceindex(), chunkoffset(), chunklength()}.
 
 -record(requestqueue, {
+    low_limit  :: non_neg_integer(),
+    high_limit :: pos_integer(),
     queue :: queue()}).
 -opaque requestqueue() :: #requestqueue{}.
 
 
-%% @doc Create an empty request queue
+%% @doc Create an empty request queue with default pipeline thresholds
 %% @end
 -spec new() -> requestqueue().
 new() ->
-    InitQueue = queue:new(),
-    #requestqueue{queue=InitQueue}.
+    new(2, 10).
+
+
+%% @doc Create an empty request queue and specify pipeline thresholds
+%% @end
+-spec new(non_neg_integer(), pos_integer()) -> requestqueue().
+new(Lowthreshold, Highthreshold) ->
+    InitQueue = #requestqueue{
+        low_limit=Lowthreshold,
+        high_limit=Highthreshold,
+        queue=queue:new()},
+    InitQueue.
+
 
 %% @doc
 %% @end
@@ -94,6 +125,26 @@ has_offset(Pieceindex, Offset, Requestqueue) ->
     end.
 
 
+%% @doc Check if the number or open requests is below the pipeline threshold
+%% @end
+-spec is_low(#requestqueue{}) -> boolean().
+is_low(Requestqueue) ->
+    #requestqueue{low_limit=Low, queue=Queue} = Requestqueue,
+    queue:len(Queue) =< Low.
+
+
+%% @doc
+%% @end
+-spec needs(#requestqueue{}) -> non_neg_integer().
+needs(Requestqueue) ->
+    #requestqueue{high_limit=High, queue=Queue} = Requestqueue,
+    Length = queue:len(Queue),
+    case Length < High of
+        true  -> High - Length;
+        false -> 0
+    end.
+
+
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
 -define(rqueue, etorrent_rqueue).
@@ -125,6 +176,25 @@ head_check_test_() ->
      ?_assertNot(?rqueue:has_offset(0, 2, Q1)),
      ?_assertNot(?rqueue:has_offset(1, 1, Q1))].
 
+low_check_test_() ->
+    Q0 = ?rqueue:new(1, 3),
+    Q1 = ?rqueue:push(0, 0, 1, Q0),
+    Q2 = ?rqueue:push(0, 1, 1, Q1),
+    [?_assert(?rqueue:is_low(Q0)),
+     ?_assert(?rqueue:is_low(Q1)),
+     ?_assertNot(?rqueue:is_low(Q2))].
+
+needs_test_() ->
+    Q0 = ?rqueue:new(1, 3),
+    Q1 = ?rqueue:push(0, 0, 1, Q0),
+    Q2 = ?rqueue:push(0, 1, 1, Q1),
+    Q3 = ?rqueue:push(0, 2, 1, Q2),
+    Q4 = ?rqueue:push(0, 3, 1, Q3),
+    [?_assertEqual(3, ?rqueue:needs(Q0)),
+     ?_assertEqual(2, ?rqueue:needs(Q1)),
+     ?_assertEqual(1, ?rqueue:needs(Q2)),
+     ?_assertEqual(0, ?rqueue:needs(Q3)),
+     ?_assertEqual(0, ?rqueue:needs(Q4))].
 
 
 -endif.
