e257d13ad6876f9a58076c6947dbc3118ac92529
Magnus Klaar <magnus.klaar@sgsstudentbostader.se>
2011-03-23 00:43:19 +0100

    Add stubs for duplication of chunk_mgr api

--- apps/etorrent/src/etorrent_endgame.erl
+++ apps/etorrent/src/etorrent_endgame.erl
@@ -14,6 +14,22 @@ init('_') -> ok.
 
 handle_call({register_peer, Pid}, _, State) ->
     %% Initialize peer data structures
+    {reply, ok, State};
+
+handle_call({request_chunks, Pid, Peerset, Numchunks}, _, State) ->
+    %% Find out which chunks are a good fit
+    %% Add Pid the set of peers having open requests for these chunks
+    %% Return the chunks to the peer
+    {reply, {ok, []}, State};
+
+handle_call({mark_fetched, Pid, Index, Offset, Length}, _ State) ->
+    %% Ensure that request_chunks does not return this chunk unless
+    %% the peer crashes
+    {reply, ok, State};
+
+handle_call({mark_stored, Pid, Index, Offset, Length}, _, State) ->
+    %% Find out which other peers have open requests for these chunks
+    %% Ensure that request_chunks never returns this request again
     {reply, ok, State}.
 
 
