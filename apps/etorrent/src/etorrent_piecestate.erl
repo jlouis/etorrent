83c7781a8fae60fa9d088a520b335b6ebf836b25
Magnus Klaar <magnus.klaar@sgsstudentbostader.se>
2011-04-03 10:51:18 +0200

    Add stored notification to piecestate

--- apps/etorrent/src/etorrent_piecestate.erl
+++ apps/etorrent/src/etorrent_piecestate.erl
@@ -2,10 +2,20 @@
 %% This module provides a common interface for processes
 %% handling piece state changes.
 
--export([valid/2]).
+-export([stored/2,
+         valid/2]).
 
 %% @doc
 %% @end
+-spec stored(non_neg_integer(), pid()) -> ok.
+stored(Piece, Srvpid) ->
+    Srvpid ! {piece, {stored, Piece}},
+    ok.
+
+
+%% @doc
+%% @end
+-spec valid(non_neg_integer(), pid()) -> ok.
 valid(Piece, Srvpid) ->
     Srvpid ! {piece, {valid, Piece}},
     ok.
@@ -13,6 +23,10 @@ valid(Piece, Srvpid) ->
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
 -define(piecestate, ?MODULE).
+
+stored_test() ->
+    ?assertEqual(ok, ?piecestate:stored(0, self())),
+    ?assertEqual({piece, {stored, 0}}, etorrent_utils:first()).
 
 valid_test() ->
     ?assertEqual(ok, ?piecestate:valid(0, self())),
