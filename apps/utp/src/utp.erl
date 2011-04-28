3a2c44887b11d9de59f98a0c37a7d8d62ccc360a
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2011-04-28 12:43:29 +0200

    Convenience test functions.

--- apps/utp/src/utp.erl
+++ apps/utp/src/utp.erl
@@ -12,6 +12,9 @@
          test_recv_large_file/1
          ]).
 
+-export([s/0,
+         r/0]).
+
 %% @doc Manual startup of the uTP application
 start(Port) ->
     ok = ensure_started([sasl, gproc, crypto]),
@@ -53,4 +56,14 @@ test_recv_large_file(Sz) ->
     {ok, Port} = gen_utp:accept(),
     {ok, R} = gen_utp:recv(Port, Sz),
     R.
+
+s() ->
+    start_app(3334),
+    {ok, Data} = file:read_file("test/utp_SUITE_data/test_large_send.dat"),
+    test_send_large_file(Data).
+
+r() ->
+    start_app(3333),
+    {ok, Data} = file:read_file("test/utp_SUITE_data/test_large_send.dat"),
+    Data = test_recv_large_file(byte_size(Data)).
 
