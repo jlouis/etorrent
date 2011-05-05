288872572fb6f6a10c1d76bc59aaf862c80b5b80
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2011-05-05 10:35:06 +0200

    Generalize the dummynet script.

--- tools/impose_network.sh
+++ tools/impose_network.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
+from="$1"
+to="$2"
+
 ipfw add 1000 allow all from any to any
 
-ipfw add 100 pipe 10 udp from 10.0.0.13 to 10.0.0.11 3333 in
-ipfw add 200 pipe 20 udp from 10.0.0.11 3333 to 10.0.0.13 out
+ipfw add 100 pipe 10 udp from ${from} to ${to} 3333 in
+ipfw add 200 pipe 20 udp from ${to} 3333 to ${from} out
  
-
+## Pipes starting with 10 is one way
 ipfw pipe 10 config bw 10Mbit/s
 ipfw pipe 10 config delay 100ms
 ipfw pipe 10 config plr 0.1
 
+## Pipes starting with 20 is the other way
 ipfw pipe 20 config bw 10Mbit/s
 ipfw pipe 20 config delay 100ms
 ipfw pipe 20 config plr 0.1
