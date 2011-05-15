5fd344061d66ef573bae3cf5f394c1020cc65d44
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2011-05-15 17:33:39 +0200

    Hinge RESET sends on an assertion that they succeed for now.

--- apps/utp/src/gen_utp.erl
+++ apps/utp/src/gen_utp.erl
@@ -224,8 +224,8 @@ handle_cast({incoming_syn, Packet, Addr, Port}, #state { listen_queue = Q,
 handle_cast({generate_reset, #packet { conn_id = ConnID,
                                        seq_no  = SeqNo }, Addr, Port},
             #state { socket = Socket } = State) ->
-    utp_socket:send_reset(Socket, Addr, Port, ConnID, SeqNo,
-                          utp_pkt:mk_random_seq_no()),
+    ok = utp_socket:send_reset(Socket, Addr, Port, ConnID, SeqNo,
+                               utp_pkt:mk_random_seq_no()),
     {noreply, State};
 handle_cast(_Msg, State) ->
     {noreply, State}.
