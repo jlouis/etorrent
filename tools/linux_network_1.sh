73fb750be11b8ec2b1cbd217b8ede2df347f1236
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2011-05-18 23:13:55 +0200

    Make the linux network test bad.

--- tools/linux_network_1.sh
+++ tools/linux_network_1.sh
@@ -5,6 +5,6 @@
 tc qdisc replace dev lo root netem \
     delay 100ms 20ms distribution normal \
     reorder 25% 50% \
-    loss 0.3% 25% \
-    duplicate 1%
+    loss 1.5% 25% \
+    duplicate 3%
 
