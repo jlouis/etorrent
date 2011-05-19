3e10d97f74f68f9bd5aa3aa34c21595e464cd483
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2011-05-19 14:44:02 +0200

    Fix a bug where the arrival of a FIN forced us into GOT_FIN
    This is troubling, but fixed: We ignored to check if the packet we got
    was really the right one in the packet order. Thus, we simply ended
    with a situation where we moved to the GOT_FIN state too early and
    thus threw out the packet that would satisfy the reorder buffer,
    blocking everyone in the process.

--- apps/utp/src/utp_pkt.erl
+++ apps/utp/src/utp_pkt.erl
@@ -308,7 +308,8 @@ handle_incoming_datagram_payload(SeqNo, Payload, PacketBuffer, State) ->
 %% sequence, it should go into the reorder buffer in the right spot.
 %% @end
 update_recv_buffer(SeqNo, <<>>,
-                   #pkt_buf { fin_state = {got_fin, SeqNo}} = PacketBuffer, _State) ->
+                   #pkt_buf { fin_state = {got_fin, SeqNo},
+                              next_expected_seq_no = SeqNo } = PacketBuffer, _State) ->
     {got_fin, PacketBuffer};
 update_recv_buffer(_SeqNo, <<>>, PB, _State) -> {ok, PB};
 update_recv_buffer(SeqNo, Payload, #pkt_buf { fin_state = {got_fin, SeqNo},
