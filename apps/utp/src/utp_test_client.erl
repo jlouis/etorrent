381d2acb855e4c544779531b2b764913cda7566c
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2011-06-01 14:25:47 +0200

    Add a test client.

---
+++ apps/utp/src/utp_test_client.erl
@@ -0,0 +1,158 @@
+%%%-------------------------------------------------------------------
+%%% @author Jesper Louis andersen <jesper.louis.andersen@gmail.com>
+%%% @copyright (C) 2011, Jesper Louis andersen
+%%% @doc
+%%%
+%%% @end
+%%% Created :  1 Jun 2011 by Jesper Louis andersen <jesper.louis.andersen@gmail.com>
+%%%-------------------------------------------------------------------
+-module(utp_test_client).
+
+-behaviour(gen_server).
+
+%% API
+-export([start_link/0,
+
+         ls/0,
+         get/2,
+         cmd/1]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
+
+-define(SERVER, ?MODULE). 
+
+-record(state, { socket :: gen_utp:socket() }).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the server
+%%
+%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
+start_link() ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+ls() ->
+    cmd(ls).
+
+get(File, Filename) ->
+    case cmd({file, File}) of
+        {ok, {filedata, Data}} ->
+            ok = file:write_file(Filename, Data);
+        {error, Reason} ->
+            {error, Reason}
+    end.
+
+cmd(Cmd) ->
+    gen_server:call(?SERVER, {cmd, Cmd}).
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Initializes the server
+%%
+%% @spec init(Args) -> {ok, State} |
+%%                     {ok, State, Timeout} |
+%%                     ignore |
+%%                     {stop, Reason}
+%% @end
+%%--------------------------------------------------------------------
+init([]) ->
+    case gen_utp:connect("port1394.ds1-vby.adsl.cybercity.dk", 3838) of
+        {ok, Socket} ->
+            {ok, #state{ socket = Socket }};
+        {error, Reason} ->
+            {stop, Reason}
+    end.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling call messages
+%%
+%% @spec handle_call(Request, From, State) ->
+%%                                   {reply, Reply, State} |
+%%                                   {reply, Reply, State, Timeout} |
+%%                                   {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, Reply, State} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_call({cmd, Cmd}, _From, #state { socket = Sock } = State) ->
+    ok = gen_utp:send_msg(Sock, Cmd),
+    case gen_utp:recv_msg(Sock) of
+        {ok, Msg} ->
+            {reply, {ok, Msg}, State};
+        {error, Reason} ->
+            {reply, {error, Reason}, State}
+    end;
+handle_call(_Request, _From, State) ->
+    Reply = ok,
+    {reply, Reply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling cast messages
+%%
+%% @spec handle_cast(Msg, State) -> {noreply, State} |
+%%                                  {noreply, State, Timeout} |
+%%                                  {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling all non call/cast messages
+%%
+%% @spec handle_info(Info, State) -> {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
+%%
+%% @spec terminate(Reason, State) -> void()
+%% @end
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Convert process state when code is changed
+%%
+%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% @end
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
