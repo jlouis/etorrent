54810f2acd45df95902d1bfcd4f082509e9658d3
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2011-07-08 15:07:44 +0200

    Document fix utp_ledbat.erl
    A slight problem was that our samples were one element too large. This
    fix makes the sizes right. While here, also document the code a bit.

--- apps/utp/src/utp_ledbat.erl
+++ apps/utp/src/utp_ledbat.erl
@@ -1,8 +1,29 @@
+%% @doc LEDBAT calculations
+%%
+%% == Overview ==
+%% The code in this module maintains a LEDBAT calculation based upon
+%% delay samples. A sample is the difference between two clocks, A and
+%% B which we assume are moving onwards at the same speed, though we
+%% do account for skew.
+%%
+%% The difference between our and their clock (A and B) is our base
+%% delay. Any deviation from this difference in the positive direction
+%% means we have a queuing delay of that amount. A deviation in the
+%% negative direction means that we have to update our base delay to
+%% the new value.
+%%
+%% == Base Delay and Current Delay ==
+%% When we take in samples, we store these samples historically. This
+%% is done such that small perturbances in the delay will not affect
+%% the greater scheme of things. We track two kinds of delays: The
+%% base history and the current (queue) history. Every minute, we
+%% update and rotate the history tables so we never latch onto a
+%% simplified view of the real world.
+%% @end
 -module(utp_ledbat).
 
--define(BASE_DELAY_HISTORY_SIZE, 13).
--define(CUR_DELAY_SIZE, 3).
--define(INITIAL_RTT_VARIANCE, 800). % ms
+-define(BASE_DELAY_HISTORY_SIZE, 12).
+-define(CUR_DELAY_SIZE, 2).
 
 -export([
          mk/1,
@@ -13,8 +34,6 @@
         ]).
 
 -record(ledbat, {
-          round_trip_time :: integer(),
-          round_trip_time_variance :: integer(),
           base_history_q :: queue(),
           delay_base :: integer(),
           last_sample :: integer(),
@@ -26,6 +45,8 @@
               t/0
              ]).
 
+%% @doc Create a new LEDBAT structure based upon the first sample
+%% @end
 mk(Sample) ->
     BaseQueue = lists:foldr(fun(_E, Q) -> queue:in(Sample, Q) end,
                             queue:new(),
@@ -34,11 +55,17 @@ mk(Sample) ->
                              queue:new(),
                              lists:seq(1, ?CUR_DELAY_SIZE)),
     #ledbat { base_history_q = BaseQueue,
-              round_trip_time_variance = ?INITIAL_RTT_VARIANCE,
               delay_base     = Sample,
               last_sample    = Sample,
               cur_delay_history_q = DelayQueue}.
 
+%% @doc Shift the base delay by an `Offset' amount.  Note that
+%% shifting by a positive value moves the base delay such that the
+%% queueing delay gets smaller. This is used in several spots to
+%% account for the fact that we just realized our queueing delay is
+%% too large. To make it smaller we shift the base delay up, which
+%% affects the queueing delay down.
+%% @end
 shift(#ledbat { base_history_q = BQ } = LEDBAT, Offset) ->
     New_Queue = queue_map(fun(E) ->
                                   utp_util:bit32(E + Offset)
@@ -46,6 +73,8 @@ shift(#ledbat { base_history_q = BQ } = LEDBAT, Offset) ->
                           BQ),
     LEDBAT#ledbat { base_history_q = New_Queue }.
 
+%% @doc Add a new sample to the LEDBAT structure
+%% @end
 add_sample(none, Sample) ->
     mk(Sample);
 add_sample(#ledbat { base_history_q = BQ,
@@ -69,6 +98,8 @@ add_sample(#ledbat { base_history_q = BQ,
                     last_sample = Sample,
                     cur_delay_history_q = N_DQ }.
 
+%% @doc Bump the internal structure by rotating the history tables
+%% @end
 clock_tick(none) ->
     none;
 clock_tick(#ledbat{ base_history_q  = BaseQ,
@@ -79,7 +110,8 @@ clock_tick(#ledbat{ base_history_q  = BaseQ,
     LEDBAT#ledbat { base_history_q = N_BaseQ,
                     delay_base = min(N_DelayBase, DelayBase) }.
 
-
+%% @doc Get out the current estimate.
+%% @end
 get_value(#ledbat { cur_delay_history_q = DelayQ }) ->
     lists:min(queue:to_list(DelayQ)).
 
@@ -122,5 +154,12 @@ compare_less(L, R) ->
 queue_map(F, Q) ->
     L = queue:to_list(Q),
     queue:from_list(lists:map(F, L)).
+
+
+
+
+
+
+
 
 
