6b0759479485388048cbd985c405a5226e2be6f9
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2011-07-23 18:52:16 +0200

    The default detail level of the et_viewer should be 90.

--- apps/utp/src/utp_filter.erl
+++ apps/utp/src/utp_filter.erl
@@ -11,6 +11,7 @@ start(ExtraOptions) ->
         [{event_order, event_ts},
          {scale, 2},
          {max_actors, 10},
+         {detail_level, 90},
          {actors, [client, timer, us, peer]},
          {trace_pattern, {utp, max}},
          {trace_global, true},
