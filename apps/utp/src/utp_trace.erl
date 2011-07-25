58413a391af8eaa52ebce9f3883167285482705d
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2011-07-25 19:36:50 +0200

    Implement tracing facilities in the tracing process.

--- apps/utp/src/utp_trace.erl
+++ apps/utp/src/utp_trace.erl
@@ -13,6 +13,8 @@
 
 %% API
 -export([
+         close_all/0,
+
          start_link/1,
          trace/3
         ]).
@@ -40,6 +42,9 @@ trace(Connection, Counter, Count) ->
     Now = erlang:now(),
     gen_server:cast(?SERVER, {trace_point, Now, Connection, Counter, Count}).
 
+close_all() ->
+    gen_server:cast(?SERVER, close_all).
+
 %%%===================================================================
 
 %% @private
@@ -64,6 +69,10 @@ handle_cast({trace_point, TimeStamp, Connection, Counter, Count},
     {N_Map, Handle} = find_handle({Connection, Counter}, Map),
     trace_message(Handle, format_message(TimeStamp, Count)),
     {noreply, State#state { map = N_Map }};
+handle_cast(close_all, #state { enabled = true, map = M }) ->
+    [file:close(H) || {_K, H} <- dict:to_list(M)],
+    #state { enabled = ture,
+             map = dict:new() };
 handle_cast(Msg, State) ->
     ?ERR([unknown_handle_case, Msg, State]),
     {noreply, State}.
@@ -82,12 +91,32 @@ code_change(_OldVsn, State, _Extra) ->
 
 %%%===================================================================
 
-find_handle({{_ConnId, _Addr, _Port}, Counter}, Map) when is_atom(Counter) ->
-    {Map, todo}.
+find_handle({Connection, Counter} = Id, Map) when is_atom(Counter) ->
+    case dict:find(Id, Map) of
+        error ->
+            find_handle(Id,
+                        dict:store({Connection, Counter},
+                                   create_handle(Connection, Counter), Map));
+        {ok, Handle} ->
+            {Map, Handle}
+    end.
 
-format_message(_Timestamp, _Count) ->
-    todo.
+format_ip({I1, I2, I3, I4}) ->
+    IS = [integer_to_list(K) || K <- [I1, I2, I3, I4]],
+    string:join(IS, ".").
 
-trace_message(_Handle, _Event) ->
-    todo.
+format_conn({ConnId, Addr, Port}) ->
+    [integer_to_list(ConnId), "-", format_ip(Addr), ":", integer_to_list(Port)].
 
+create_handle(Connection, Counter) ->
+    FName = [os:getpid(), "-", format_conn(Connection), "-", atom_to_list(Counter)],
+    {ok, Handle} = file:open(FName, [write, raw, binary, {delayed_write, 8192, 3000}]),
+    Handle.
+
+format_message({Mega, Secs, Micro}, Count) ->
+    Time = [Mega*1000000+Secs, ".", io_lib:format("~6..0B", [Micro])],
+    CountS = integer_to_list(Count),
+    [Time, $|, CountS, $\n].
+
+trace_message(Handle, Event) ->
+    ok = file:write(Handle, Event).
