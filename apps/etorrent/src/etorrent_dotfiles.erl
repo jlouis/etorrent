9043aff1c016791ac9f3be270235803c363aae26
Magnus Klaar <magnus.klaar@gmail.com>
2011-08-10 17:59:03 +0200

    add info_path and info_hash functions

--- apps/etorrent/src/etorrent_dotfiles.erl
+++ apps/etorrent/src/etorrent_dotfiles.erl
@@ -5,7 +5,9 @@
 %% exported functions
 -export([make/0,
          torrents/0,
-         copy/1]).
+         copy_file/1,
+         info_path/1,
+         info_hash/1]).
 
 %% private functions
 -export([exists/1]).
@@ -37,15 +39,32 @@ torrents() ->
 
 %% @doc Make a private copy of a torrent file.
 %% @end
--spec copy(Torrentfile::string()) -> ok.
-copy(Torrentfile) when is_list(Torrentfile) ->
-    File = filename:basename(Torrentfile),
-    Dotdir = gproc:get_env(l, etorrent, dotdir),
-    Dest = filename:join([Dotdir, File]),
+-spec copy_file(Torrentfile::string()) -> ok.
+copy_file(Torrentfile) when is_list(Torrentfile) ->
+    Dest = copy_path(Torrentfile),
     case file:copy(Torrentfile, Dest) of
         {ok, _} -> ok;
         {error, _}=Error -> Error
     end.
+
+-spec copy_path(Torrentfile::string()) -> Copyfile::string().
+copy_path(Torrentfile) ->
+    File = filename:basename(Torrentfile),
+    Dotdir = gproc:get_env(l, etorrent, dotdir),
+    filename:join([Dotdir, File]).
+
+%% @doc Get the path of the info file for a torrent.
+%% @end
+-spec info_path(Torrentfile::string()) -> Infofile::string().
+info_path(Torrentfile) when is_list(Torrentfile) ->
+    copy_path(Torrentfile) ++ ".info".
+
+%% @doc Get the info hash of a torrent file.
+%% @end
+-spec info_hash(Torrentfile::string()) -> binary().
+info_hash(Torrentfile) ->
+    {ok, Torrent} = etorrent_bcoding:parse_file(Torrentfile),
+    etorrent_metainfo:get_infohash(Torrent).
 
 %% @private Check if a file path exists.
 -spec exists(Path::string()) -> boolean().
@@ -70,6 +89,15 @@ teardown_config(_Dir) ->
     %% @todo Recursive file:delete.
     ok.
 
+testpath() ->
+    "../../../test/etorrent_eunit_SUITE_data/debian-6.0.2.1-amd64-netinst.iso.torrent".
+
+testfile() ->
+    "debian-6.0.2.1-amd64-netinst.iso.torrent".
+
+testhash() ->
+    <<142,215,218,181,31,70,216,236,194,208,141,204,28,28,160,136,237,138,83,180>>.
+
 
 dotfiles_test_() ->
     {setup,local,
@@ -83,7 +111,9 @@ dotfiles_test_() ->
             {setup,local,
                 fun() -> ?MODULE:make() end,
                 fun(_) -> ok end, [
-                ?_test(test_copy_torrent())
+                ?_test(test_copy_torrent()),
+                ?_test(test_info_filename()),
+                ?_test(test_info_hash())
             ]}
         ]}
     ]}.
@@ -97,8 +127,14 @@ test_ensure_exists() ->
     ?assert(?MODULE:exists(gproc:get_env(l, etorrent, dotdir))).
 
 test_copy_torrent() ->
-    Torrent = "../../../test/etorrent_eunit_SUITE_data/debian-6.0.2.1-amd64-netinst.iso.torrent",
-    ok = ?MODULE:copy(Torrent),
-    ?assertEqual({ok, ["debian-6.0.2.1-amd64-netinst.iso.torrent"]}, ?MODULE:torrents()).
+    ok = ?MODULE:copy_file(testpath()),
+    ?assertEqual({ok, [testfile()]}, ?MODULE:torrents()).
+
+test_info_filename() ->
+    ok = ?MODULE:copy_file(testpath()),
+    ?assertEqual({ok, [testfile()]}, ?MODULE:torrents()).
+
+test_info_hash() ->
+    ?assertEqual(testhash(), ?MODULE:info_hash(testpath())).
 
 -endif.
