084c9f1506bee2070d2d9cf579444844472517d6
Magnus Klaar <magnus.klaar@gmail.com>
2011-08-27 17:13:25 +0200

    update etorrent_rlimit to use rlimit:atake

--- apps/etorrent/src/etorrent_rlimit.erl
+++ apps/etorrent/src/etorrent_rlimit.erl
@@ -2,7 +2,7 @@
 %% This module wraps the rlimit application and exports functions that
 %% are expected to be called from etorrent_peer_send and etorrent_peer_recv.
 %%
-%% etorrent_peer_send is expected to aquire a slot before a message is sent.
+%% etorrent_peer_send is expected to aquire a slot after a message is sent.
 %% etorrent_peer_recv is expected to aquire a slot after receiving a message.
 -export([init/0, send/1, recv/1]).
 
@@ -21,17 +21,19 @@ init() ->
     ok = rlimit:new(?UPLOAD, to_byte_rate(ULRate), 1000).
 
 %% @doc Aquire a send slot.
+%% A continue message will be sent to the caller once a slot has been aquired.
 %% @end
 -spec send(non_neg_integer()) -> ok.
 send(Bytes) ->
-    rlimit:take(Bytes, ?UPLOAD).
+    rlimit:atake(Bytes, {rlimit, continue}, ?UPLOAD).
 
 
 %% @doc Aquire a receive slot.
+%% A continue message will be sent to the caller once a slot has been aquired.
 %% @end
 -spec recv(non_neg_integer()) -> ok.
 recv(Bytes) ->
-    rlimit:take(Bytes, ?DOWNLOAD).
+    rlimit:atake(Bytes, {rlimit, continue}, ?DOWNLOAD).
 
 %% @private Convert KB/s to B/s
 to_byte_rate(infinity) ->
