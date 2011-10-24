751b37db6fa52758fab343b6b63d28d5601cbefe
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2011-10-24 21:54:43 +0200

    Postpone startup of the Progress manager.
    We now postpone the startup of the Progress manager until we now what
    pieces a torrent has and has not. This fixes a problem with negative
    left values since it was always set up as if we had downloaded 0
    pieces. The negative left values occur due to a responsibility failure
    in the code: the progress manager should be responsible, but it is
    calculated separately.

--- apps/etorrent/src/etorrent_torrent_ctl.erl
+++ apps/etorrent/src/etorrent_torrent_ctl.erl
@@ -112,11 +112,9 @@ init([Parent, Id, {Torrent, TorrentFile, TorrentIH}, PeerId]) ->
 %% @private
 %% @todo Split and simplify this monster function
 initializing(timeout, #state{id=Id, torrent=Torrent, hashes=Hashes} = S0) ->
-    Progress = etorrent_progress:await_server(Id),
     Pending  = etorrent_pending:await_server(Id),
     Endgame  = etorrent_endgame:await_server(Id),
     S = S0#state{
-        progress=Progress,
         pending=Pending,
         endgame=Endgame},
 
@@ -159,6 +157,14 @@ initializing(timeout, #state{id=Id, torrent=Torrent, hashes=Hashes} = S0) ->
             etorrent_table:statechange_torrent(Id, started),
             etorrent_event:started_torrent(Id),
 
+            %% Start the progress manager
+            {ok, ProgressPid} =
+                etorrent_torrent_sup:start_progress(
+                  S#state.parent_pid,
+                  Id,
+                  Torrent,
+                  ValidPieces),
+
             %% Start the tracker
             {ok, TrackerPid} =
                 etorrent_torrent_sup:start_child_tracker(
@@ -168,10 +174,8 @@ initializing(timeout, #state{id=Id, torrent=Torrent, hashes=Hashes} = S0) ->
                   S#state.peer_id,
                   Id),
 
-            %% Since the process will now go to a state where it won't do anything
-            %% for a long time, GC it.
-            garbage_collect(),
-            NewState = S#state{tracker_pid=TrackerPid, valid=ValidPieces},
+            NewState = S#state{tracker_pid=TrackerPid, valid=ValidPieces,
+                               progress = ProgressPid },
             {next_state, started, NewState}
     end.
 
