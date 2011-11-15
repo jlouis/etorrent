0e141c9ea2ac963d273a8f249b9c7ac6bbe6be5e
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2011-11-15 20:29:22 +0100

    Clean up the etorrent module.

--- apps/etorrent/src/etorrent.erl
+++ apps/etorrent/src/etorrent.erl
@@ -19,7 +19,7 @@
 -export([start/1, start/2]).
 
 -ignore_xref([{h, 0}, {l, 0}, {s, 0}, {s, 1}, {check, 1},
-	      {help, 0}, {list, 0}, {show, 0}, {show, 1}]).
+              {help, 0}, {list, 0}, {show, 0}, {show, 1}]).
 
 %%====================================================================
 
@@ -45,18 +45,19 @@ start(Filename) when is_list(Filename) ->
     etorrent_ctl:start(Filename).
 
 %% @doc Start downloading torrent given by Filename
-%% When the torrent is completed, the Callback function is executed
-%% in its own newly spawned process. If the Callback crashes, it will do so
-%% with an error logged message, but the Callback itself is responsible for detecting
-%% that this is the case and install an eventual exception/exit handler on its
-%% execution so it can be caught.
+%% When the torrent is completed, the Callback function is executed in
+%% its own newly spawned process. If the Callback crashes, it will do
+%% so with an error logged message, but the Callback itself is
+%% responsible for detecting that this is the case and install an
+%% eventual exception/exit handler on its execution so it can be
+%% caught.
 %% @end
 start(Filename, {Ref, Pid})
   when is_list(Filename), is_reference(Ref), is_pid(Pid) ->
     start(Filename, fun() ->
-			    ?INFO([completing_torrent_callback, Filename]),
-			    Pid ! {Ref, done}
-		    end);
+                            ?INFO([completing_torrent_callback, Filename]),
+                            Pid ! {Ref, done}
+                    end);
 start(Filename, CallBack) when is_list(Filename), is_function(CallBack, 0) ->
     etorrent_ctl:start(Filename, CallBack).
 
@@ -70,10 +71,10 @@ start(Filename, CallBack) when is_list(Filename), is_function(CallBack, 0) ->
 list() ->
     All = etorrent_query:torrent_list(),
     A   = lists:sort(
-	    fun(PL1, PL2) ->
-		    proplists:get_value(id, PL1) =< proplists:get_value(id, PL2)
-	    end,
-	    All),
+            fun(PL1, PL2) ->
+                    proplists:get_value(id, PL1) =< proplists:get_value(id, PL2)
+            end,
+            All),
     {DownloadRate, UploadRate} = etorrent_peer_states:get_global_rate(),
     io:format("~3s ~11s ~11s ~11s ~11s ~3s ~3s ~7s~n",
               ["Id:", "total", "left", "uploaded", "downloaded",
@@ -81,23 +82,24 @@ list() ->
 
     lists:foreach(
       fun (R) ->
-	      Eta = etorrent_rate:format_eta(proplists:get_value(left, R),
-					     DownloadRate),
-	      Uploaded = proplists:get_value(uploaded, R) +
-			 proplists:get_value(all_time_uploaded, R),
-	      Downloaded = proplists:get_value(downloaded, R) +
-		           proplists:get_value(all_time_downloaded, R),
-	      {value, PL} = etorrent_table:get_torrent(proplists:get_value(id, R)),
+              Eta = etorrent_rate:format_eta(proplists:get_value(left, R),
+                                             DownloadRate),
+              Uploaded = proplists:get_value(uploaded, R) +
+                  proplists:get_value(all_time_uploaded, R),
+              Downloaded = proplists:get_value(downloaded, R) +
+                  proplists:get_value(all_time_downloaded, R),
+              {value, PL} =
+                  etorrent_table:get_torrent(proplists:get_value(id, R)),
               io:format("~3.B ~11.B ~11.B ~11.B ~11.B ~3.B ~3.B ~7.3f% ~s ~n",
                         [proplists:get_value(id, R),
-			 proplists:get_value(total, R),
-			 proplists:get_value(left, R),
-			 Uploaded,
-			 Downloaded,
-			 proplists:get_value(leechers, R),
-			 proplists:get_value(seeders, R),
+                         proplists:get_value(total, R),
+                         proplists:get_value(left, R),
+                         Uploaded,
+                         Downloaded,
+                         proplists:get_value(leechers, R),
+                         proplists:get_value(seeders, R),
                          percent_complete(R),
-			 Eta]),
+                         Eta]),
               io:format("    ~s~n", [proplists:get_value(filename, PL)])
       end, A),
     io:format("Rate Up/Down: ~8.2f / ~8.2f~n", [UploadRate / 1024.0,
@@ -111,12 +113,12 @@ show() ->
 %% @doc Show detailed information for Item.
 %% @end
 show(Item) when is_integer(Item) ->
-    %{atomic, Torrent} = etorrent_torrent:select(Item),
+    %% {atomic, Torrent} = etorrent_torrent:select(Item),
     case etorrent_table:get_torrent(Item) of
         {value, PL} ->
             io:format("Id: ~3.B Name: ~s~n",
                       [proplists:get_value(id, PL),
-		       proplists:get_value(filename, PL)]);
+                       proplists:get_value(filename, PL)]);
         not_found ->
             io:format("No such torrent Id~n")
     end;
@@ -139,8 +141,9 @@ help() ->
                 {"list, l", "List torrents in system"},
                 {"show, s", "Show detailed information for a given torrent"},
                 {"stop", "Stop the system"},
-	        {"start(File)", "Start the given file as a .torrent"},
-	        {"start(File, CBFun)", "Start .torrent with a callback function upon completion"}],
+                {"start(File)", "Start the given file as a .torrent"},
+                {"start(File, CBFun)",
+                 "Start .torrent with a callback function upon completion"}],
 
     lists:foreach(fun({Command, Desc}) ->
                           io:format("~-20.s - ~s~n", [Command, Desc])
@@ -166,12 +169,3 @@ percent_complete(R) ->
     T = proplists:get_value(total, R),
     L = proplists:get_value(left, R),
     (T - L) / T * 100.
-
-
-
-
-
-
-
-
-
